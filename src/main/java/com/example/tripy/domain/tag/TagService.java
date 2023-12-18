b6bd6e9804cca4812ba585d6adbc46b44c93c7b2
tjdgns8439 <tjdgns8439@inha.edu>
2023-12-18 22:47:43 +0900

    Feat: Tag 객체 매핑

---
+++ src/main/java/com/example/tripy/domain/tag/TagService.java
@@ -0,0 +1,9 @@
+package com.example.tripy.domain.tag;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TagService {
+}
