08c1c3d36f092a73a7d3b4e6227888b6475ee649
tjdgns8439 <tjdgns8439@inha.edu>
2024-01-16 20:00:43 +0900

    Feat: 화폐(환율) 조회 구현

--- src/main/java/com/example/tripy/domain/currency/CurrencyService.java
+++ src/main/java/com/example/tripy/domain/currency/CurrencyService.java
@@ -1,9 +1,38 @@
 package com.example.tripy.domain.currency;
 
+import com.example.tripy.domain.country.Country;
+import com.example.tripy.domain.country.CountryRepository;
+import com.example.tripy.domain.currency.dto.CurrencyResponseDto;
+import com.example.tripy.global.common.response.code.status.ErrorStatus;
+import com.example.tripy.global.common.response.exception.GeneralException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CurrencyService {
+
+
+    private final CurrencyRepository currencyRepository;
+    private final CountryRepository countryRepository;
+
+    public List<CurrencyResponseDto> getCurrencyList() {
+        List<Currency> currencyList = currencyRepository.findAll();
+        return currencyList.stream()
+            .map(CurrencyResponseDto::toDTO)
+            .collect(Collectors.toList());
+    }
+
+    public CurrencyResponseDto getCurrencyByCountryId(Long countryId) {
+        Country country = countryRepository.findById(countryId)
+            .orElseThrow(() -> new GeneralException(ErrorStatus._EMPTY_COUNTRY));
+
+        Currency currency = currencyRepository.findByCountryName(country.getName())
+            .orElseThrow(() -> new GeneralException(
+                ErrorStatus._EMPTY_CURRENCY));
+
+        return CurrencyResponseDto.toDTO(currency);
+    }
 }
