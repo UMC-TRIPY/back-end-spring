b7698e5eb584f8ddde6b4dcfd3b23024ab705bbf
Minhyeok Song <minhyeok.dev@gmail.com>
2024-01-23 13:53:14 +0900

    Refactor: #14 URL 변수 수정
    - memberId @RequestParam -> @PathVariable 로 수정

--- src/main/java/com/example/tripy/domain/bag/BagController.java
+++ src/main/java/com/example/tripy/domain/bag/BagController.java
@@ -6,6 +6,7 @@ import com.example.tripy.global.common.response.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class BagController {
      */
     @GetMapping("travel-bag/member/bag/{memberId}")
     public ApiResponse<PageResponseDto<List<BagSimpleInfo>>> getBagsList(
-        @RequestParam(value = "memberId") Long memberId, @RequestParam(value = "page") int page,
+        @PathVariable(value = "memberId") Long memberId, @RequestParam(value = "page") int page,
         @RequestParam(value = "size") int size) {
         return ApiResponse.onSuccess(bagService.getBagsList(page, size, memberId));
     }
