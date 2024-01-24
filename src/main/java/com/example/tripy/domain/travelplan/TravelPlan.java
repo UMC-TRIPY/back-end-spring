553f814eb53691adba6811cacaaed8c1f1d151c4
Minhyeok Song <minhyeok.dev@gmail.com>
2024-01-25 00:18:31 +0900

    Feat: #25 updateBagExists 함수 추가
    - 가방 존재 -> false, 존재X -> true로 변경 메서드

--- src/main/java/com/example/tripy/domain/travelplan/TravelPlan.java
+++ src/main/java/com/example/tripy/domain/travelplan/TravelPlan.java
@@ -38,4 +38,8 @@ public class TravelPlan extends BaseTimeEntity {
     private Member member;
 
 
+    public void updateBagExists(){
+        //가방이 존재하지 않으면 true로 변경, 존재하면 false로 변경
+        this.bagExists = !this.bagExists;
+    }
 }
