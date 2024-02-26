a88f6d8268f910529ff9dcb4e400aa61f1316d7f
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-02-26 21:09:11 +0900

    Feat: 여행지별 준비물 불러오기 구현 (#33)
    * Feat: #32 - 여행지별 준비물 불러오기 구현
    
    * Feat: #32 - URL 복수형 변경
    
    * Feat: #32 - Dto 변환 및 countryId 추가

--- src/main/java/com/example/tripy/domain/bag/BagController.java
+++ src/main/java/com/example/tripy/domain/bag/BagController.java
@@ -1,6 +1,8 @@
 package com.example.tripy.domain.bag;
 
 import com.example.tripy.domain.bag.dto.BagResponseDto.BagListSimpleInfo;
+import com.example.tripy.domain.countrymaterial.CountryMaterialService;
+import com.example.tripy.domain.material.dto.MaterialResponseDto.MaterialListByCountry;
 import com.example.tripy.global.common.dto.PageResponseDto;
 import com.example.tripy.global.common.response.ApiResponse;
 import java.util.List;
@@ -17,26 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/travel-bag")
 public class BagController {
 
-    private final BagService bagService;
+	private final BagService bagService;
+	private final CountryMaterialService countryMaterialService;
 
+	/**
+	 * [GET] 내 여행 가방 모두 불러오기
+	 */
+	@GetMapping("/members/bags/{memberId}")
+	public ApiResponse<PageResponseDto<List<BagListSimpleInfo>>> getBagsList(
+		@PathVariable(value = "memberId") Long memberId, @RequestParam(value = "page") int page,
+		@RequestParam(value = "size") int size) {
+		return ApiResponse.onSuccess(bagService.getTravelBagExistsList(page, size, memberId));
+	}
 
-    /**
-     * [GET] 내 여행 가방 모두 불러오기
-     */
-    @GetMapping("/member/bag/{memberId}")
-    public ApiResponse<PageResponseDto<List<BagListSimpleInfo>>> getBagsList(
-        @PathVariable(value = "memberId") Long memberId, @RequestParam(value = "page") int page,
-        @RequestParam(value = "size") int size) {
-        return ApiResponse.onSuccess(bagService.getTravelBagExistsList(page, size, memberId));
-    }
+	/**
+	 * [POST] 내 여행 일정 목록에서 해당하는 가방 목록 생성하기
+	 */
+	@PostMapping("/members/bags/{memberId}/{travelPlanId}")
+	public ApiResponse<String> updateBagExists(@PathVariable Long memberId,
+		@PathVariable(value = "travelPlanId") Long travelPlanId) {
+		return ApiResponse.onSuccess(bagService.updateBagExists(memberId, travelPlanId));
+	}
 
-    /**
-     * [POST] 내 여행 일정 목록에서 해당하는 가방 목록 생성하기
-     */
-    @PostMapping("/member/bag/{memberId}/{travelPlanId}")
-    public ApiResponse<String> updateBagExists(@PathVariable Long memberId,
-        @PathVariable(value = "travelPlanId") Long travelPlanId) {
-        return ApiResponse.onSuccess(bagService.updateBagExists(memberId, travelPlanId));
-    }
+	@GetMapping("/material-name")
+	public ApiResponse<MaterialListByCountry> getCountryMaterials(
+		@RequestParam(value = "countryName") String countryName) {
+		return ApiResponse.onSuccess(countryMaterialService.getCountryMaterials(countryName));
+	}
 
 }
