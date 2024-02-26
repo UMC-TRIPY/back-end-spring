56134234ce5e1480362b46ea42c738cdcad6dd93
SongMinHyeok <minhyeok.dev@gmail.com>
2024-02-26 17:47:27 +0900

    Feat: #32 - 여행지별 준비물 불러오기 구현

--- src/main/java/com/example/tripy/domain/countrymaterial/CountryMaterialService.java
+++ src/main/java/com/example/tripy/domain/countrymaterial/CountryMaterialService.java
@@ -1,9 +1,18 @@
 package com.example.tripy.domain.countrymaterial;
 
+import com.example.tripy.domain.material.Material;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CountryMaterialService {
+
+	private final CountryMaterialRepository countryMaterialRepository;
+
+	public List<Material> getCountryMaterials(String countryName) {
+		List<Material> materials = countryMaterialRepository.findMaterialsByCountry(countryName);
+		return materials;
+	}
 }
