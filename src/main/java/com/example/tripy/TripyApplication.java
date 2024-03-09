2bef060fb0dec0605872599b935c32a84e4a49cb
gourderased <gourderased@gmail.com>
2024-03-09 21:28:05 +0900

    Fix: basetimeentity 적용

--- src/main/java/com/example/tripy/TripyApplication.java
+++ src/main/java/com/example/tripy/TripyApplication.java
@@ -2,10 +2,12 @@ package com.example.tripy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 public class TripyApplication {
 
     public static void main(String[] args) {
