e300a1345b7c66da8bdc33f9de080b70ab9b2c43
SongMinHyeok <minhyeok.dev@gmail.com>
2024-03-09 16:27:22 +0900

    Feat: #34 - BagResponseDto 추가
    - BagListWithMaterialInfo 추가

--- src/main/java/com/example/tripy/domain/bag/dto/BagResponseDto.java
+++ src/main/java/com/example/tripy/domain/bag/dto/BagResponseDto.java
@@ -1,5 +1,7 @@
 package com.example.tripy.domain.bag.dto;
 
+import com.example.tripy.domain.bag.Bag;
+import com.example.tripy.domain.bagmaterials.dto.BagMaterialsResponseDto.BagMaterialInfo;
 import com.example.tripy.domain.travelplan.TravelPlan;
 import java.util.Date;
 import java.util.List;
@@ -10,23 +12,41 @@ import lombok.NoArgsConstructor;
 
 public class BagResponseDto {
 
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class BagListSimpleInfo {
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class BagListSimpleInfo {
 
-        private Date departure;
-        private Date arrival;
-        private List<String> cities;
-        private Long travelPlanId;
+		private Date departure;
+		private Date arrival;
+		private List<String> cities;
+		private Long travelPlanId;
 
-        @Builder
-        public BagListSimpleInfo(List<String> cities, TravelPlan travelPlan) {
-            this.departure = travelPlan.getDeparture();
-            this.arrival = travelPlan.getArrival();
-            this.cities = cities;
-            this.travelPlanId = travelPlan.getId();
-        }
+		@Builder
+		public BagListSimpleInfo(List<String> cities, TravelPlan travelPlan) {
+			this.departure = travelPlan.getDeparture();
+			this.arrival = travelPlan.getArrival();
+			this.cities = cities;
+			this.travelPlanId = travelPlan.getId();
+		}
 
-    }
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class BagListWithMaterialInfo {
+
+		private String bagName;
+		private List<BagMaterialInfo> bagMaterials;
+		private Long travelPlanId;
+
+		@Builder
+		public BagListWithMaterialInfo(Bag bag, List<BagMaterialInfo> bagMaterials) {
+			this.bagName = bag.getBagName();
+			this.bagMaterials = bagMaterials;
+			this.travelPlanId = bag.getTravelPlan().getId();
+		}
+
+	}
 }
