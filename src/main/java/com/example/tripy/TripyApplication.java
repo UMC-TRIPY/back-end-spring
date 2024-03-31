633d09a36cfa4ecc964323e2e224294855a73a7f
tjdgns8439 <tjdgns8439@inha.edu>
2024-03-31 22:19:09 +0900

    Feat: Swagger 주소 설정

--- src/main/java/com/example/tripy/TripyApplication.java
+++ src/main/java/com/example/tripy/TripyApplication.java
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(servers = {
-    @Server(url = "/", description = "Default Server URL")
+    @Server(url = "${spring.swagger.domain}", description = "Default Server URL")
 })
 @EnableScheduling
 @SpringBootApplication
@@ -19,4 +22,17 @@ public class TripyApplication {
         SpringApplication.run(TripyApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .maxAge(3000);
+            }
+        };
+    }
 }
