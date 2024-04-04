92f55a5c8637a7107574f5f04ae16f3dc4ce8a9e
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-04-05 02:36:03 +0900

    Refactor: 가방 API 멤버 관련 수정
    * Refactor: #48 - memberId 삭제 및 주석 추가
    
    * Refactor: #48 - 스웨거 어노테이션 추가
    
    * Refactor: #48 - 스웨거 어노테이션 추가

--- src/main/java/com/example/tripy/domain/bag/BagController.java
+++ src/main/java/com/example/tripy/domain/bag/BagController.java
@@ -7,6 +7,10 @@ import com.example.tripy.domain.countrymaterial.CountryMaterialService;
 import com.example.tripy.domain.material.dto.MaterialResponseDto.MaterialListByCountry;
 import com.example.tripy.global.common.dto.PageResponseDto;
 import com.example.tripy.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "여행 가방 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/travel-bag")
@@ -28,7 +33,10 @@ public class BagController {
 	/**
 	 * [GET] 내 여행 가방 모두 불러오기
 	 */
-	@GetMapping("/members/bags/{memberId}")
+	@Operation(summary = "내 여행 가방 모두 불러오기", description = "여행 가방이 존재하는 목록을 불러옵니다.")
+	@Parameter(name = "page", description = "내 여행 가방 목록 페이지 번호, query string 입니다.")
+	@Parameter(name = "size", description = "내 여행 가방 목록 페이지 번호, query string 입니다.")
+	@GetMapping("/members/bags")
 	public ApiResponse<PageResponseDto<List<BagListSimpleInfo>>> getBagsList(
 		@RequestParam(value = "page") int page, @RequestParam(value = "size") int size) {
 		return ApiResponse.onSuccess(bagService.getTravelBagExistsList(page, size));
@@ -37,32 +45,45 @@ public class BagController {
 	/**
 	 * [POST] 내 여행 일정 목록에서 해당하는 가방 목록 생성하기
 	 */
+	@Operation(summary = "내 여행 일정 목록에서 해당하는 가방 목록 생성하기", description = "여행 일정 목록에서 가방을 생성하기 위해 해당하는 가방 목록을 생성합니다.")
+	@Parameter(name = "travelPlanId", description = "여행 계획 Id, Path Variable 입니다.")
 	@PostMapping("/members/bags/{travelPlanId}")
 	public ApiResponse<String> updateBagExists(
 		@PathVariable(value = "travelPlanId") Long travelPlanId) {
 		return ApiResponse.onSuccess(bagService.updateBagExists(travelPlanId));
 	}
 
+	/**
+	 * [GET] 여행지별 준비물 불러오기
+	 */
+	@Operation(summary = "여행지별 준비물 불러오기", description = "나라별 존재하는 추천 준비물을 불러옵니다.")
+	@Parameter(name = "countryName", description = "나라 이름, query string 입니다.")
 	@GetMapping("/material-name")
 	public ApiResponse<MaterialListByCountry> getCountryMaterials(
 		@RequestParam(value = "countryName") String countryName) {
 		return ApiResponse.onSuccess(countryMaterialService.getCountryMaterials(countryName));
 	}
 
+	/**
+	 * [POST] 여행 일정에 해당하는 개별 가방 생성(추가)하기
+	 */
+	@Operation(summary = "여행 일정에 해당하는 개별 가방 생성(추가)하기", description = "여행 일정에 해당하는 캐리어, 크로스백과 같은 가방을 생성합니다.")
+	@Parameter(name = "travelPlanId", description = "여행 계획 Id, query string 입니다.")
 	@PostMapping("/members/bags")
 	public ApiResponse<String> createBag(@RequestBody CreateBagRequest createBagRequest,
 		@RequestParam Long travelPlanId) {
 		return ApiResponse.onSuccess(bagService.addBag(createBagRequest, travelPlanId));
-  }
+	}
 
 	/**
 	 * [GET] 여행 가방 리스트와 가방 내 준비물 불러오기
 	 */
-	@GetMapping("/members/{memberId}/materials/{travelPlanId}")
+	@Operation(summary = "내 여행 가방 리스트와 가방 내 준비물 불러오기", description = "여행 가방 리스트와 그 가방에 존재하는 준비물을 불러오고, isChecked로 챙겼는지 확인합니다.")
+	@Parameter(name = "travelPlanId", description = "여행 계획 Id, Path Variable 입니다.")
+	@GetMapping("/members/materials/{travelPlanId}")
 	public ApiResponse<List<BagListWithMaterialInfo>> getBagsListAndMaterialsByTravelPlan(
-		@PathVariable Long memberId, @PathVariable(value = "travelPlanId") Long travelPlanId) {
-		return ApiResponse.onSuccess(
-			bagService.getBagsListAndMaterialsByTravelPlan(memberId, travelPlanId));
-  }
+		@PathVariable(value = "travelPlanId") Long travelPlanId) {
+		return ApiResponse.onSuccess(bagService.getBagsListAndMaterialsByTravelPlan(travelPlanId));
+	}
 
 }
