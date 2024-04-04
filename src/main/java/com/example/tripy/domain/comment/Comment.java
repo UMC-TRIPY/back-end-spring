ba0f2a1abe2d6f189c051298240e211e098de2de
gourderased <117848386+gourderased@users.noreply.github.com>
2024-04-05 02:35:05 +0900

    Feat: 댓글 조회/작성 구현
    * Feat: 댓글 작성, 조회 구현
    
    * Fix: 디렉토리 에러 해결
    
    * Fix: 게시글 조회 방식 수정
    
    * Fix: dto 수정
    
    * Fix: 날짜 표기 변경

---
+++ src/main/java/com/example/tripy/domain/comment/Comment.java
@@ -0,0 +1,45 @@
+package com.example.tripy.domain.comment;
+
+import com.example.tripy.domain.member.Member;
+import com.example.tripy.domain.post.Post;
+import com.example.tripy.global.utils.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Comment extends BaseTimeEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Post post;
+
+    public static Comment toEntity(Member member, Post post, String content) {
+        return Comment.builder()
+            .member(member)
+            .post(post)
+            .content(content)
+            .build();
+    }
+}
