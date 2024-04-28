8e05f81c5e39f094306af07d6dd2e86b74ef27c7
yerim216 <bing_01@naver.com>
2024-04-28 16:14:31 +0900

    feat: Member 엔티티 implements UserDetails(Spring Security), payload 함수 추가

--- src/main/java/com/example/tripy/domain/member/Member.java
+++ src/main/java/com/example/tripy/domain/member/Member.java
@@ -2,23 +2,26 @@ package com.example.tripy.domain.member;
 
 import com.example.tripy.domain.member.enums.SocialType;
 import com.example.tripy.global.utils.BaseTimeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Builder
-public class Member extends BaseTimeEntity {
-
+public class Member extends BaseTimeEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,5 +40,41 @@ public class Member extends BaseTimeEntity {
 
     private SocialType socialType;
 
+    @Column(length = 100, nullable = false, unique = true)
+    private String keyCode; //로그인 식별키
 
+    public String getPayload(){
+        return this.getId()+"tripy";
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return keyCode;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
