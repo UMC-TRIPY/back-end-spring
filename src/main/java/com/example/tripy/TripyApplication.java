925ae6d7a08760f72d685d97e489121cc792dd99
yerim216 <bing_01@naver.com>
2024-05-21 01:34:09 +0900

    feat: createdAt, updatedAt 자동 업데이트를 위한 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/example/tripy/TripyApplication.java
+++ src/main/java/com/example/tripy/TripyApplication.java
@@ -3,9 +3,11 @@ package com.example.tripy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class TripyApplication {
 
     public static void main(String[] args) {
