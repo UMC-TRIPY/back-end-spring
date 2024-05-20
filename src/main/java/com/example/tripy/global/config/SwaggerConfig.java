7b30e23ac814e648896c8255e34c3e62c1b96596
yerim216 <bing_01@naver.com>
2024-05-21 01:34:38 +0900

    feat: swagger JWT Bearer 인증버튼 설정

--- src/main/java/com/example/tripy/global/config/SwaggerConfig.java
+++ src/main/java/com/example/tripy/global/config/SwaggerConfig.java
@@ -3,22 +3,39 @@ package com.example.tripy.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    String jwtSchemeName = "JWT TOKEN";
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement())
+                .components(components());
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("Tripy Swagger")
+                .description("트리피 Swagger 입니다!")
                 .version("1.0.0");
+    }
+    private Components components(){
+        return new Components()
+            .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                .name(jwtSchemeName)
+                .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+    }
+
+    private SecurityRequirement securityRequirement(){
+        return new SecurityRequirement().addList(jwtSchemeName);
     }
 }
