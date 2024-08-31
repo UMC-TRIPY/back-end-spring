67e6726d114850559ad6c1cb175283eabd6b2d0c
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-01 01:39:11 +0900

    Feat: #65 - Add findTravelTimePlansByTravelPlan method to TravelTimePlanRepository

--- src/main/java/com/example/tripy/domain/traveltimeplan/TravelTimePlanRepository.java
+++ src/main/java/com/example/tripy/domain/traveltimeplan/TravelTimePlanRepository.java
@@ -1,6 +1,11 @@
 package com.example.tripy.domain.traveltimeplan;
 
+import com.example.tripy.domain.travelplan.TravelPlan;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TravelTimePlanRepository extends JpaRepository<TravelTimePlan, Long> {
+
+    List<TravelTimePlan> findTravelTimePlansByTravelPlan(TravelPlan travelPlan);
+
 }
