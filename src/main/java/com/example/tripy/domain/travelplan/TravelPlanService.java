9a6cc253e6696f5331e0bb7c35707e2ac9a91e51
SongMinHyeok <minhyeok.dev@gmail.com>
2024-08-31 22:23:40 +0900

    Feat: #65 - 여행 목록 조회 로직 구현
    - `getTravelPlanExistsList` 구현
    - `setTravelPlanListSimpleInfo` 구현

--- src/main/java/com/example/tripy/domain/travelplan/TravelPlanService.java
+++ src/main/java/com/example/tripy/domain/travelplan/TravelPlanService.java
@@ -1,11 +1,18 @@
 package com.example.tripy.domain.travelplan;
 
+import com.example.tripy.domain.cityplan.CityPlanRepository;
 import com.example.tripy.domain.cityplan.CityPlanService;
 import com.example.tripy.domain.member.Member;
 import com.example.tripy.domain.travelplan.dto.TravelPlanRequestDto.CreateTravelPlanRequest;
+import com.example.tripy.domain.travelplan.dto.TravelPlanResponse.GetTravelPlanListSimpleInfo;
 import com.example.tripy.domain.travelplan.dto.TravelPlanResponse.GetTravelPlanSimpleInfo;
+import com.example.tripy.global.common.PageResponseDto;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +22,7 @@ public class TravelPlanService {
 
     private final TravelPlanRepository travelPlanRepository;
     private final CityPlanService cityPlanService;
+    private final CityPlanRepository cityPlanRepository;
 
     @Transactional
     public GetTravelPlanSimpleInfo addTravelPlan(Member member,
@@ -24,6 +32,34 @@ public class TravelPlanService {
         List<String> cityLists = createTravelPlanRequest.getCityNameList();
         cityPlanService.addCityPlan(cityLists, travelPlan);
         return GetTravelPlanSimpleInfo.toDto(travelPlan.getId());
+    }
+
+    public PageResponseDto<List<GetTravelPlanListSimpleInfo>> getTravelPlanExistsList(int page,
+        int size, Member member) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<TravelPlan> result = travelPlanRepository.findAllByMemberAndBagExistsIsFalse(member,
+            pageable);
+
+        return new PageResponseDto<>(result.getNumber(), result.getTotalPages(),
+            setTravelPlanListSimpleInfo(result.stream().collect(
+                Collectors.toList())));
+    }
+
+    public List<GetTravelPlanListSimpleInfo> setTravelPlanListSimpleInfo(
+        List<TravelPlan> travelPlans) {
+        return travelPlans.stream()
+            .map(travelPlan -> {
+                List<String> cities = cityPlanRepository.findAllByTravelPlan(travelPlan)
+                    .stream()
+                    .map(cityPlan -> cityPlan.getCity().getName())
+                    .collect(Collectors.toList());
+
+                return GetTravelPlanListSimpleInfo.toDto(travelPlan, cities
+                );
+            })
+            .collect(Collectors.toList());
+
     }
 
 }
