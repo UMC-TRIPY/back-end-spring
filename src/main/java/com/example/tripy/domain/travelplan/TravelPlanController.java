673b9919b55d5846658aa242ec323d2f1127255a
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-01 01:11:57 +0900

    Feat: #65 - 내 여행 목록 조회 라우트 /members에서 /로 변경

--- src/main/java/com/example/tripy/domain/travelplan/TravelPlanController.java
+++ src/main/java/com/example/tripy/domain/travelplan/TravelPlanController.java
@@ -41,7 +41,7 @@ public class TravelPlanController {
     @Operation(summary = "내 여행 목록 조회하기", description = "내가 속한 여행 목록을 조회힙니다. (일정에서 bag_exists 값이 false 값 조회)")
     @Parameter(name = "page", description = "페이징 page, query string 입니다.")
     @Parameter(name = "size", description = "페이징 size, query string 입니다.")
-    @GetMapping("/members")
+    @GetMapping("/")
     public ApiResponse<PageResponseDto<List<GetTravelPlanListSimpleInfo>>> getTravelPlanList(
         @CurrentUser Member member, @RequestParam(value = "page") int page,
         @RequestParam(value = "size") int size) {
