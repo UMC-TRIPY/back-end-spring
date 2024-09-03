93868ac4e2fc9e9324f4a773c1b0959ea6735a69
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:29:12 +0900

    Feat: #65 - PlanFriendController 추가
    - 일정 공유 중인 친구 조회 API

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
@@ -1,8 +1,29 @@
 package com.example.tripy.domain.planfriend;
 
+import com.example.tripy.domain.member.Member;
+import com.example.tripy.domain.planfriend.dto.PlanFriendResponseDto.GetPlanFriendInfo;
+import com.example.tripy.global.response.ApiResponse;
+import com.example.tripy.global.security.CurrentUser;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RequiredArgsConstructor
+@Tag(name = "친구 계획 API")
+@RequestMapping("/api/plan-friends")
 @RestController
 public class PlanFriendController {
+
+    private final PlanFriendService planFriendService;
+
+    @GetMapping("/{travelPlanId}")
+    public ApiResponse<List<GetPlanFriendInfo>> getPlanFriends(@CurrentUser Member member,
+        @PathVariable(value = "travelPlanId") Long travelPlanId) {
+        return ApiResponse.onSuccess(planFriendService.getPlanFriends(member, travelPlanId));
+    }
+
 }
