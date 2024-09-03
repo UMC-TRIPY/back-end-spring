afed05afd3ba75f4c4572b7616aabe73df2cc794
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 15:55:48 +0900

    Feat: #65 - TravelTimePlanRepository 메서드 추가
    - `findByTravelPlanAndId` 추가

--- src/main/java/com/example/tripy/domain/traveltimeplan/TravelTimePlanRepository.java
+++ src/main/java/com/example/tripy/domain/traveltimeplan/TravelTimePlanRepository.java
@@ -2,10 +2,12 @@ package com.example.tripy.domain.traveltimeplan;
 
 import com.example.tripy.domain.travelplan.TravelPlan;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TravelTimePlanRepository extends JpaRepository<TravelTimePlan, Long> {
 
     List<TravelTimePlan> findTravelTimePlansByTravelPlan(TravelPlan travelPlan);
 
+    Optional<TravelTimePlan> findByTravelPlanAndId(TravelPlan travelPlan, Long travelTimePlanId);
 }
