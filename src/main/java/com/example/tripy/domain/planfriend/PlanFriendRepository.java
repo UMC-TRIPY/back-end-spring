23f5fa813d62ec3c0eacdf27b5fbc01757ef3de1
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:28:47 +0900

    Feat: #65 - findPlanFriendsByTravelPlan 메서드 추가

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendRepository.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendRepository.java
@@ -1,6 +1,12 @@
 package com.example.tripy.domain.planfriend;
 
+import com.example.tripy.domain.travelplan.TravelPlan;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlanFriendRepository extends JpaRepository<PlanFriend, Long> {
+
+    List<PlanFriend> findPlanFriendsByTravelPlan(TravelPlan travelPlan);
+
+
 }
