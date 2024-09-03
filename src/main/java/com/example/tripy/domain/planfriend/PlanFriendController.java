1a7f99cd220675474969e8f3c2967f77fe5d49ae
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:31:38 +0900

    Docs: #65 - 스웨거 설명 추가

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
@@ -4,6 +4,8 @@ import com.example.tripy.domain.member.Member;
 import com.example.tripy.domain.planfriend.dto.PlanFriendResponseDto.GetPlanFriendInfo;
 import com.example.tripy.global.response.ApiResponse;
 import com.example.tripy.global.security.CurrentUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,8 @@ public class PlanFriendController {
 
     private final PlanFriendService planFriendService;
 
+    @Operation(summary = "일정 공유 친구 조회하기", description = "여행 일정을 공유 중인 친구들을 조회합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 일정 ID, Path Variable 입니다.")
     @GetMapping("/{travelPlanId}")
     public ApiResponse<List<GetPlanFriendInfo>> getPlanFriends(@CurrentUser Member member,
         @PathVariable(value = "travelPlanId") Long travelPlanId) {
