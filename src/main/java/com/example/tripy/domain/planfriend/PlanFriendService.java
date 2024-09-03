dc7a4c7b34ef849cea9836c4a3112b06f05dc2a5
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:28:34 +0900

    Feat: #65 - 일정 공유 중인 친구 조회 로직 구현

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendService.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendService.java
@@ -1,9 +1,32 @@
 package com.example.tripy.domain.planfriend;
 
+import com.example.tripy.domain.member.Member;
+import com.example.tripy.domain.planfriend.dto.PlanFriendResponseDto.GetPlanFriendInfo;
+import com.example.tripy.domain.travelplan.TravelPlan;
+import com.example.tripy.domain.travelplan.TravelPlanRepository;
+import com.example.tripy.global.response.code.status.ErrorStatus;
+import com.example.tripy.global.response.exception.GeneralException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class PlanFriendService {
+
+    private final PlanFriendRepository planFriendRepository;
+    private final TravelPlanRepository travelPlanRepository;
+
+    public List<GetPlanFriendInfo> getPlanFriends(Member member, Long travelPlanId) {
+
+        TravelPlan travelPlan = travelPlanRepository.findByMemberAndId(member, travelPlanId)
+            .orElseThrow(() -> new GeneralException(ErrorStatus._EMPTY_TRAVEL_PLAN));
+
+        List<PlanFriend> planFriends = planFriendRepository.findPlanFriendsByTravelPlan(travelPlan);
+
+        return planFriends.stream()
+            .map(GetPlanFriendInfo::toDTO)
+            .toList();
+    }
+
 }
