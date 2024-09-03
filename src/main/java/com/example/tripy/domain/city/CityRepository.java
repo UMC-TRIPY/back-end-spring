a0eb4b9e3c2d8afb8822f98d8e948b75a45f7089
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 17:16:39 +0900

    Style: #65 - 코딩 컨벤션 및 import 문 정리

--- src/main/java/com/example/tripy/domain/city/CityRepository.java
+++ src/main/java/com/example/tripy/domain/city/CityRepository.java
@@ -1,10 +1,9 @@
 package com.example.tripy.domain.city;
 
-import io.swagger.v3.oas.annotations.Operation;
 import java.util.Optional;
-import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
     Optional<City> findByName(String cityName);
 }
