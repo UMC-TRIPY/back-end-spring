a91e91c5cffc0404f751935870f2dc31c2d9042e
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 15:48:25 +0900

    Feat: #65 - TravelPlanController 추가
    - 상세 일정 추가,수정,삭제,조회

--- src/main/java/com/example/tripy/domain/travelplan/TravelPlanController.java
+++ src/main/java/com/example/tripy/domain/travelplan/TravelPlanController.java
@@ -4,7 +4,9 @@ import com.example.tripy.domain.member.Member;
 import com.example.tripy.domain.travelplan.dto.TravelPlanRequestDto.CreateTravelPlanRequest;
 import com.example.tripy.domain.travelplan.dto.TravelPlanResponse.GetTravelPlanListSimpleInfo;
 import com.example.tripy.domain.travelplan.dto.TravelPlanResponse.GetTravelPlanSimpleInfo;
+import com.example.tripy.domain.traveltimeplan.dto.TravelTimePlanRequestDto.CreateTravelTimePlanRequest;
 import com.example.tripy.domain.traveltimeplan.dto.TravelTimePlanResponseDto.GetTravelTimePlanDetailInfo;
+import com.example.tripy.domain.traveltimeplan.dto.TravelTimePlanResponseDto.GetTravelTimePlanSimpleInfo;
 import com.example.tripy.global.common.PageResponseDto;
 import com.example.tripy.global.response.ApiResponse;
 import com.example.tripy.global.security.CurrentUser;
@@ -13,7 +15,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -52,10 +56,55 @@ public class TravelPlanController {
     //TODO : Date 값 이상하게 들어가는 현상 해결 필요
     @Operation(summary = "여행 일정 상세 조회하기", description = "여행 목록을 상세 조회힙니다.")
     @GetMapping("/{travelPlanId}")
-    public ApiResponse<List<GetTravelTimePlanDetailInfo>> getTravelPlanDetail(
+    public ApiResponse<List<GetTravelTimePlanSimpleInfo>> getTravelPlanDetail(
         @CurrentUser Member member, @PathVariable(value = "travelPlanId") Long travelPlanId) {
         return ApiResponse.onSuccess(travelPlanService.getTravelPlanDetail(member, travelPlanId));
+    }
 
+    @Operation(summary = "여행 상세 일정 추가하기", description = "여행 상세 일정을 추가합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 계획 id")
+    @PostMapping("/{travelPlanId}/detail")
+    public ApiResponse<GetTravelTimePlanSimpleInfo> createTravelTimePlan(@CurrentUser Member member,
+        @RequestBody CreateTravelTimePlanRequest createTravelTimePlanRequest,
+        @PathVariable(value = "travelPlanId") Long travelPlanId) {
+        return ApiResponse.onSuccess(
+            travelPlanService.addTravelTimePlan(member, travelPlanId, createTravelTimePlanRequest));
+    }
+
+    @Operation(summary = "여행 상세 일정 수정하기", description = "여행 상세 일정을 수정합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 계획 id")
+    @Parameter(name = "travelTimePlanId", description = "여행 상세 일정 id")
+    @PatchMapping("/{travelPlanId}/detail/{travelTimePlanId}")
+    public ApiResponse<GetTravelTimePlanSimpleInfo> updateTravelTimePlan(@CurrentUser Member member,
+        @RequestBody CreateTravelTimePlanRequest createTravelTimePlanRequest,
+        @PathVariable(value = "travelPlanId") Long travelPlanId,
+        @PathVariable(value = "travelTimePlanId") Long travelTimePlanId) {
+        return ApiResponse.onSuccess(
+            travelPlanService.updateTravelTimePlan(member, travelPlanId, travelTimePlanId,
+                createTravelTimePlanRequest));
+    }
+
+    @Operation(summary = "여행 상세 일정 삭제하기", description = "여행 일정을 삭제합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 계획 id")
+    @Parameter(name = "travelTimePlanId", description = "여행 일정 id")
+    @DeleteMapping("/{travelPlanId}/detail/{travelTimePlanId}")
+    public ApiResponse<String> deleteTravelTimePlan(@CurrentUser Member member,
+        @PathVariable(value = "travelPlanId") Long travelPlanId,
+        @PathVariable(value = "travelTimePlanId") Long travelTimePlanId) {
+        return ApiResponse.onSuccess(
+            travelPlanService.deleteTravelTimePlan(member, travelPlanId, travelTimePlanId));
+    }
+
+    @Operation(summary = "여행 상세 일정 상세 조회하기", description = "여행 상세 일정을 상세 조회합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 계획 id")
+    @Parameter(name = "travelTimePlanId", description = "여행 상세 일정 id")
+    @GetMapping("{travelPlanId}/detail/{travelTimePlanId}")
+    public ApiResponse<GetTravelTimePlanDetailInfo> getTravelTimePlanDetail(
+        @CurrentUser Member member,
+        @PathVariable(value = "travelPlanId") Long travelPlanId,
+        @PathVariable(value = "travelTimePlanId") Long travelTimePlanId) {
+        return ApiResponse.onSuccess(
+            travelPlanService.getTravelTimePlanDetail(member, travelPlanId, travelTimePlanId));
     }
 
 }
