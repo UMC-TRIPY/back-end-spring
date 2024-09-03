ac0f7433e5abe2e2c40209e345101defe92ddf18
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 17:16:11 +0900

    Feat: #65 - 도시 검색, 도시 목록 조회 로직 구현

--- src/main/java/com/example/tripy/domain/city/CityService.java
+++ src/main/java/com/example/tripy/domain/city/CityService.java
@@ -1,9 +1,31 @@
 package com.example.tripy.domain.city;
 
+import com.example.tripy.domain.city.dto.CityResponseDto.CityFilterResult;
+import com.example.tripy.domain.city.dto.CityResponseDto.SearchCityResponse;
+import com.example.tripy.global.response.code.status.ErrorStatus;
+import com.example.tripy.global.response.exception.GeneralException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CityService {
+
+    private final CityRepository cityRepository;
+
+    public SearchCityResponse searchCity(String cityName) {
+        return SearchCityResponse.toDTO(cityRepository.findByName(cityName)
+            .orElseThrow(() -> new GeneralException(ErrorStatus._EMPTY_CITY)));
+    }
+
+    public List<CityFilterResult> getCities(String cityName, String countryName) {
+        return cityRepository.findAll().stream()
+            .filter(city -> cityName == null || city.getName().equals(cityName))
+            .filter(city -> countryName == null || city.getCountry().getName().equals(countryName))
+            .map(CityFilterResult::toDTO)
+            .collect(Collectors.toList());
+    }
+
 }
