3a6b454f1c644057846496306efd13456955b443
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 17:19:27 +0900

    Feat: #65 - 국가 목록 조회 로직 구현

--- src/main/java/com/example/tripy/domain/country/CountryService.java
+++ src/main/java/com/example/tripy/domain/country/CountryService.java
@@ -1,9 +1,24 @@
 package com.example.tripy.domain.country;
 
+import com.example.tripy.domain.country.dto.CountryResponseDto.CountryFilterResult;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public List<CountryFilterResult> getCountries(String countryName, String continentName) {
+        return countryRepository.findAll().stream()
+            .filter(country -> countryName == null || country.getName().equals(countryName))
+            .filter(country -> continentName == null || country.getContinent().getName()
+                .equals(continentName))
+            .map(CountryFilterResult::toDTO)
+            .collect(Collectors.toList());
+    }
+
 }
