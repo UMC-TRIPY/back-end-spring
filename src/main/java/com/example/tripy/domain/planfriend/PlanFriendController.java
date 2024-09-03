5f47c172fb656ee3e062a6c4ab8a9aef81c1e449
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:43:32 +0900

    Feat: #65 - 일정 공유 친구 추가하기 API 추가

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendController.java
@@ -1,6 +1,7 @@
 package com.example.tripy.domain.planfriend;
 
 import com.example.tripy.domain.member.Member;
+import com.example.tripy.domain.planfriend.dto.PlanFriendRequestDto.CreatePlanFriendRequest;
 import com.example.tripy.domain.planfriend.dto.PlanFriendResponseDto.GetPlanFriendInfo;
 import com.example.tripy.global.response.ApiResponse;
 import com.example.tripy.global.security.CurrentUser;
@@ -11,6 +12,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,16 @@ public class PlanFriendController {
     public ApiResponse<List<GetPlanFriendInfo>> getPlanFriends(@CurrentUser Member member,
         @PathVariable(value = "travelPlanId") Long travelPlanId) {
         return ApiResponse.onSuccess(planFriendService.getPlanFriends(member, travelPlanId));
+    }
+
+    @Operation(summary = "일정 공유 친구 추가하기", description = "여행 일정을 공유할 친구를 추가합니다.")
+    @Parameter(name = "travelPlanId", description = "여행 일정 ID, Path Variable 입니다.")
+    @PostMapping("/{travelPlanId}")
+    public ApiResponse<String> addPlanFriend(@CurrentUser Member member,
+        @PathVariable(value = "travelPlanId") Long travelPlanId,
+        @RequestBody CreatePlanFriendRequest createPlanFriendRequest) {
+        return ApiResponse.onSuccess(
+            planFriendService.addPlanFriend(member, travelPlanId, createPlanFriendRequest));
     }
 
 }
