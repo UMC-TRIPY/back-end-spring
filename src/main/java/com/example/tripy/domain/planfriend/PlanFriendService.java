910d3fe2fc00bc9b9a9172fbaaa70e70c619d815
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-03 19:43:12 +0900

    Feat: #65 - 일정 공유 친구 추가하기 로직 구현

--- src/main/java/com/example/tripy/domain/planfriend/PlanFriendService.java
+++ src/main/java/com/example/tripy/domain/planfriend/PlanFriendService.java
@@ -1,6 +1,8 @@
 package com.example.tripy.domain.planfriend;
 
 import com.example.tripy.domain.member.Member;
+import com.example.tripy.domain.member.MemberRepository;
+import com.example.tripy.domain.planfriend.dto.PlanFriendRequestDto.CreatePlanFriendRequest;
 import com.example.tripy.domain.planfriend.dto.PlanFriendResponseDto.GetPlanFriendInfo;
 import com.example.tripy.domain.travelplan.TravelPlan;
 import com.example.tripy.domain.travelplan.TravelPlanRepository;
@@ -16,6 +18,7 @@ public class PlanFriendService {
 
     private final PlanFriendRepository planFriendRepository;
     private final TravelPlanRepository travelPlanRepository;
+    private final MemberRepository memberRepository;
 
     public List<GetPlanFriendInfo> getPlanFriends(Member member, Long travelPlanId) {
 
@@ -27,6 +30,21 @@ public class PlanFriendService {
         return planFriends.stream()
             .map(GetPlanFriendInfo::toDTO)
             .toList();
+    }
+
+    public String addPlanFriend(Member member, Long travelPlanId,
+        CreatePlanFriendRequest createPlanFriendRequest) {
+
+        Member friend = memberRepository.findById(createPlanFriendRequest.getMemberId())
+            .orElseThrow(() -> new GeneralException(ErrorStatus._EMPTY_MEMBER));
+
+        TravelPlan travelPlan = travelPlanRepository.findByMemberAndId(member, travelPlanId)
+            .orElseThrow(() -> new GeneralException(ErrorStatus._EMPTY_TRAVEL_PLAN));
+
+        PlanFriend planFriend = PlanFriend.toEntity(travelPlan, friend);
+        planFriendRepository.save(planFriend);
+
+        return "친구 추가 성공";
     }
 
 }
